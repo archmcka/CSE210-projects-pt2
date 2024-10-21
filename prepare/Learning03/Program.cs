5a751cc86e0b05033ea33b2952c1304dd7af6a8b
Mckayla Archibald <arc22002@byui.edu>
2024-10-21 16:47:21 -0600

    my preperation for learning 5

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1=new Fraction();
+        Console.WriteLine(f1.GetNumbersFraction());
+        Console.WriteLine(f1.GetDecimal());
+
+        Fraction f2=new Fraction();
+        Console.WriteLine(f2.GetNumbersFraction());
+        Console.WriteLine(f2.GetDecimal());
+
+        Fraction f3=new Fraction();
+        Console.WriteLine(f3.GetNumbersFraction());
+        Console.WriteLine(f3.GetDecimal());
+
+        Fraction f4=new Fraction();
+        Console.WriteLine(f4.GetNumbersFraction());
+        Console.WriteLine(f4.GetDecimal());
     }
 }
