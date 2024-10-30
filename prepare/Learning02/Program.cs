eb3aa61898e70da6878e34e916ff34054f6c5dd5
Mckayla Archibald <arc22002@byui.edu>
2024-10-30 12:53:48 -0600

    my learning 4 project

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Security.AccessControl;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1= new Job();
+        job1._company="microsoft";
+        job1._jobTitle="software engineer";
+        job1._startDate="2019";
+        job1._endDate="2024";
+
+        Job job2= new Job();
+        job2._company="apple";
+        job2._jobTitle="manager";
+        job2._startDate="2011";
+        job2._endDate="2019";
+
+        Resume myResume =d new Resume();
+        myResume._name="mckayla archibald";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
+
     }
 }
+
