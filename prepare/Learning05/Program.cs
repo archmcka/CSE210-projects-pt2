0ef02d45cae5dfa1cb9a76d10532c05bff41d73d
Mckayla Archibald <arc22002@byui.edu>
2024-11-20 11:41:14 -0700

    the program part of leanrning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Console.WriteLine("pleace pick which shape you want to know more about ");
+        // Console.WriteLine("1. square  2. rectangle  3. circle");
+        // string userinput = Console.ReadLine();
+        // switch (userinput)
+        // {
+        //     case "1":   
+        //         Square 
+        // }
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("blue",4);
+        Shape.Add(s1); 
+
+        Rectangle r1 = new Rectangle("green", 3,4);
+        Shape.Add(s2);
+
+        Circle c1 = new Circle("yellow",4);
+        Shape.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+        }
+
     }
 }
